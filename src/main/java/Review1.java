ca6d5361a07fb27513220849e91ad8f588981fe6
cindy-pan-ics4u1-02-25 <cindy.pan@student.tdsb.on.ca>
2025-02-04 15:10:30 +0000

    Update Review1

--- src/main/java/Review1.java
+++ src/main/java/Review1.java
@@ -2,22 +2,39 @@ public class Review1 {
 
 	public static void q1() {
 		//Write question 1 code here
+		//Output the sentence “There once was a man from St. Ives.”
+		System.out.println("There once was a man from St. Ives");
 	}
 
 	public static void q2() {
 		//Write question 2 code here
+		//Create two integer variables with the values 9 and 5.
+		// Output the quotient (rounded down) and the remainder on separate lines.
+		int numNine = 9;
+		int numFive = 5;
+		System.out.println (10%numNine);
+		System.out.println (10%numFive);
 	}
 
 	public static void q3() {
 		//Write question 3 code here
+		//Write code that will produce the following printout using only a single println().
+		System.out.println("hello \n hello again");
 	}
 
 	public static void q4() {
 		//Write question 4 code here
+		boolean trueFalse = true;
+		System.out.println(trueFalse);
 	}
 
 	public static void q5() {
 		//Write question 5 code here
+		//Create two Strings. The first should have the value "Hello", the second should have the value "World".
+		//Hello World on the same line using two print statements with a space between them.
+		String one = "Hello";
+		String two = "world!";
+		System.out.println(one +" "+ two);
 	}
 
 }
