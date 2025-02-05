3a83e1280af7f8870409a0a804d11475956ad49a
cindy-pan-ics4u1-02-25 <cindy.pan@student.tdsb.on.ca>
2025-02-05 14:18:33 +0000

    Update Review1

--- src/main/java/Review1.java
+++ src/main/java/Review1.java
@@ -3,7 +3,7 @@ public class Review1 {
 	public static void q1() {
 		//Write question 1 code here
 		//Output the sentence “There once was a man from St. Ives.”
-		System.out.println("There once was a man from St. Ives");
+		System.out.println("There once was a man from St. Ives.");
 	}
 
 	public static void q2() {
@@ -12,14 +12,14 @@ public class Review1 {
 		// Output the quotient (rounded down) and the remainder on separate lines.
 		int numNine = 9;
 		int numFive = 5;
-		System.out.println (10%numNine);
-		System.out.println (10%numFive);
+		System.out.println (1%numFive);
+		System.out.println (13%numNine);
 	}
 
 	public static void q3() {
 		//Write question 3 code here
 		//Write code that will produce the following printout using only a single println().
-		System.out.println("hello \n hello again");
+		System.out.println("Hello\nHello again");
 	}
 
 	public static void q4() {
@@ -33,7 +33,7 @@ public class Review1 {
 		//Create two Strings. The first should have the value "Hello", the second should have the value "World".
 		//Hello World on the same line using two print statements with a space between them.
 		String one = "Hello";
-		String two = "world!";
+		String two = "World";
 		System.out.println(one +" "+ two);
 	}
 
